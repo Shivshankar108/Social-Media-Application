7170db8be80360dff3a10dccee351155ccb31080
Shiv Shankar <shankarshiv470@gamil.com>
2024-12-24 16:29:59 +0530

    debugged user class

--- social-media-app/src/main/java/com/example/social_media_app/entity/User.java
+++ social-media-app/src/main/java/com/example/social_media_app/entity/User.java
@@ -27,7 +27,7 @@ import lombok.Setter;
 public class User {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Long id;
 	private String firstName;
 	private String lastName;
